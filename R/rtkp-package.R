ccc932d0d4a3f8652e16cbece92a77f4a570c852
Kingsman-Key <15077877013@163.com>
2024-12-04 09:43:09 +0800

    First trial of commit

---
+++ R/rtkp-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom magrittr %>%
+#' @importFrom rms datadist
+#' @importFrom rms lrm
+#' @importFrom rms ols
+## usethis namespace: end
+NULL
